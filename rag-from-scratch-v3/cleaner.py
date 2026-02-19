93e62e178b8176e7b39ffc247b203b625fde8d0d
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-02-19 19:11:23 +0530

    Add files via upload

---
+++ rag-from-scratch-v3/cleaner.py
@@ -0,0 +1,8 @@
+import re
+
+def clean_text(text: str) -> str:
+    text = text.replace("\n", " ")
+    text = re.sub(r"-\s+", "", text)
+    text = re.sub(r"\s+", " ", text)
+
+    return text.strip()
